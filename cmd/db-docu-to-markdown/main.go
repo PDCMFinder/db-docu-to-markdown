08007845cb194b0f6eb98c6c09eff1aa9956060a
Mauricio Martinez Jimenez <mmartinez@ebi.ac.uk>
2023-06-14 20:16:16 +0100

    Add comments

--- cmd/db-docu-to-markdown/main.go
+++ cmd/db-docu-to-markdown/main.go
@@ -1,3 +1,24 @@
+/*
+db-docu-to-markdown creates markdown files with basic information about a database.
+
+USAGE:
+
+	db-docu-to-markdown [global options] command [command options] [arguments...]
+
+COMMANDS:
+
+	help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+
+	--host value, -H value                                   database host (default: "localhost")
+	--port value, -P value                                   database port (default: 8080)
+	--user value, -u value                                   database user (default: "admin")
+	--password value, -p value                               database password (default: "password")
+	--name value, -n value                                   database name (default: "test")
+	--schemas value, -s value [ --schemas value, -s value ]  comma separated list of schemas to describe (default: "public")
+	--dbtype value, --dt value                               specify the database type (default: "postgres")
+*/
 package main
 
 import (
@@ -19,8 +40,8 @@ func main() {
 	var dbtype string
 
 	app := &cli.App{
-		Name:  "db-docu-sync-confluence",
-		Usage: "syncronises one or more pages in confluence with database documentation",
+		Name:  "db-docu-to-markdown",
+		Usage: "creates markdown files with basic information about a database",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:        "host",
--- internal/generation/generator_service.go
+++ internal/generation/generator_service.go
@@ -1,3 +1,4 @@
+// Package generation orchestrates the logic to connect to the database and create the markdown files with the extracted documentation.
 package generation
 
 import (
@@ -5,6 +6,7 @@ import (
 	"github.com/PDCMFinder/db-descriptor/pkg/service"
 )
 
+// Extracts the documentation from the database and creates the markdown files
 func GenerateMarkdown(dbDescriptorInput connector.Input) {
 	// Get the database description
 	databaseDescription := service.GetDbDescription(dbDescriptorInput)
